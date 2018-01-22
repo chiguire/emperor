5bf3a6dc7c52bcf6877ba1f22853e6e9fafc221e
Ciro Durán <ciro.duran@gmail.com>
2018-01-22 07:57:15 +0000

    Some code

--- public/game.js
+++ public/game.js
@@ -1,10 +1,41 @@
-// client-side js
-// run by the browser each time your view template is loaded
-
-// by default, you've got jQuery,
-// add other scripts at the bottom of index.html
 
 $(function() {
+
+  var emperor = function() {
+    
+    // Game States
+    addingPlayersGameState: "ADDING_PLAYERS_GAME_STATE",
+    playerNominatesGameState: "PLAYER_NOMINATES_GAME_STATE",
+    redHerringNominatesGameState: "RED_HERRING_NOMINATES_GAME_STATE",
+    playGameState: "PLAY_GAME_STATE",
+
+    gameStart: function() {
+      return {
+        players: [],
+        state: this.addingPlayersGameState
+      };
+    },
+
+    addPlayerName: function(gameState, playerName) {
+      // Check name is not already in list
+      gameState.players.push({
+        name: playerName,
+        nominates: ""
+      });
+    }
+
+    removePlayerName: function(gameState, playerName) {
+      var players = gameState.players;
+      var playerIndex = players.indexOf(playerName);
+      //if playerIndex == -1 throw exception
+      //split players
+      gameState.players = players;
+    }
+
+    nominatePlayer: function(gameState, playerName, nominates) {
+      var 
+    }
+  }();
   console.log('hello world :o');
   
   $.get('/dreams', function(dreams) {
@@ -24,3 +55,4 @@ $(function() {
   });
 
 });
+
